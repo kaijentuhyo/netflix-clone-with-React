fdb294ab599168ac24274e527bb59033b9fd6b2e
kaijentuhyo <kaijentuhyo@gmail.com>
2025-03-30 01:07:59 +0600

    Expanded movie card collection on the home page.

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -3,7 +3,7 @@ import './TitleCards.css'
 import cards_data from '../../assets/cards/Cards_data'
 
 
-const TitleCards = () => {
+const TitleCards = ({title, category}) => {
 
   const cardsRef = useRef();
 
@@ -18,7 +18,7 @@ const TitleCards = () => {
 
   return (
     <div className='title-cards'>
-      <h2>Popular on Netflix</h2>
+      <h2>{title?title:"Popular on Netflix"}</h2>
       <div className="card-list" ref={cardsRef}>
         {cards_data.map((card, index) => {
           return <div className="card" key={index}>
