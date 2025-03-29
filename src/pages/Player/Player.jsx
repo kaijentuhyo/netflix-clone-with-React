4ad264e3f6e8de3db2bc282759e1d2434f663d1c
kaijentuhyo <kaijentuhyo@gmail.com>
2025-03-30 04:27:30 +0600

    Implemented media player for movie playback.

--- src/pages/Player/Player.jsx
+++ src/pages/Player/Player.jsx
@@ -1,10 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Player.css'
+import back_arrow_icon from '../../assets/back_arrow_icon.png'
+import { useParams } from 'react-router-dom'
 
 export const Player = () => {
+
+  const {id} = useParams();
+
+  const [apiData, setApiData] = useState({
+    name: "",
+    key: "",
+    published_at: "",
+    typeof: ""
+  })
+
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjMzZkNjU4MTM4ODg1YzQwMzZkYmFhMzIyZTA0ZjJkYiIsIm5iZiI6MTc0MzI4MjUyNy41Nzc5OTk4LCJzdWIiOiI2N2U4NjE1Zjg3MTQ1ZGUzMmQ2MzkxZDAiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.1Um-yfzBgBo5kWI5fhAtxp2zSEbAI_9LyIgJpS7QPf8'
+    }
+  };
+
+  useEffect(() => {
+    fetch(`https://api.themoviedb.org/3/movie/${id}/videos?language=en-US`, options)
+      .then(res => res.json())
+      .then(res => setApiData(res.results[0]))
+      .catch(err => console.error(err));
+  }, [])
+
   return (
     <div className='player'>
-
+      <img src={back_arrow_icon} alt="" />
+      <iframe width='90%' height='90%' src={`https://www.youtube.com/embed/${apiData.key}`} title='trailer' frameBorder='0' allowFullScreen></iframe>
+      <div className="player-info">
+        <p>{apiData.published_at.slice(0,10)}</p>
+        <p>{apiData.name}</p>
+        <p>{apiData.type}</p>
+      </div>
     </div>
   )
 }
