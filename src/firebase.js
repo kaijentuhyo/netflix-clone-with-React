e487de3c4b976e629420b4c76870de1a75bbe5f3
kaijentuhyo <kaijentuhyo@gmail.com>
2025-03-30 08:13:55 +0600

    Integrated Firebase Authentication and Firestore for user management and data storage.

---
+++ src/firebase.js
@@ -0,0 +1,47 @@
+import { initializeApp } from "firebase/app";
+import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword, signOut } from "firebase/auth";
+import { addDoc, collection, getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCN6WSwTiKmyL-jGr8cfEyNDkLyrJGYN1M",
+  authDomain: "netflix-clone-ad767.firebaseapp.com",
+  projectId: "netflix-clone-ad767",
+  storageBucket: "netflix-clone-ad767.firebasestorage.app",
+  messagingSenderId: "968270655044",
+  appId: "1:968270655044:web:b7ac4daf198c22c8531611"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+const signup = async (name, email, password)=>{
+    try {
+        const res = await createUserWithEmailAndPassword(auth, email, password);
+        const user = res.user;
+        await addDoc(collection(db, "user"), {
+            uid: user.uid,
+            name,
+            authProvider: "local",
+            email,
+        });
+    } catch (error) {
+        console.log(error);
+        alert(error);
+    }
+}
+
+const login = async (email, password)=>{
+    try {
+        await signInWithEmailAndPassword(auth, email, password);
+    } catch (error) {
+        console.log(error);
+        alert(error);
+    }
+}
+
+const logout = ()=>{
+    signOut(auth);
+}
+
+export {auth, db, login, signup, logout};
